4bb7eab4361eef524b6e9ffd33eafc391c496ece
github keita7777 <github mine0kei@gmail.com>
2024-11-17 04:10:00 +0900

    ブックマーク作成ボタンコンポーネント

--- app/(bookmark)/page.tsx
+++ app/(bookmark)/page.tsx
@@ -1,8 +1,10 @@
+import BookmarkButtons from "@/components/Bookmark/BookmarkButtons";
 import BookmarkList from "@/components/Bookmark/BookmarkList";
 
 export default function Home() {
   return (
     <>
+      <BookmarkButtons />
       <BookmarkList />
     </>
   );
---
+++ components/Bookmark/BookmarkButtons.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { FaPlus } from "react-icons/fa";
+
+type BookmarkButtonsProps = {
+  folderId?: string;
+};
+
+const BookmarkButtons = ({ folderId }: BookmarkButtonsProps) => {
+  return (
+    <div className="mb-5 flex flex-col xl:flex-row justify-between">
+      <div>
+        <Link
+          href={
+            folderId
+              ? `/create-bookmark?folderId=${folderId}`
+              : "/create-bookmark"
+          }
+          className="flex justify-between items-center gap-2 w-72 bg-gray-500 text-white px-8 py-2 rounded-md text-xl"
+        >
+          <span>ブックマーク作成</span>
+          <FaPlus />
+        </Link>
+      </div>
+      <div className="flex gap-4">
+        <select
+          name=""
+          id=""
+          className="border border-black rounded-md text-xl px-4 py-2"
+        >
+          <option value="">5件表示</option>
+          <option value="">10件表示</option>
+          <option value="">全て表示</option>
+        </select>
+        <select
+          name=""
+          id=""
+          className="border border-black rounded-md text-xl px-4 py-2"
+        >
+          <option value="">並び替え</option>
+          <option value="">アクセス数降順</option>
+          <option value="">アクセス数昇順</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+export default BookmarkButtons;
